07ab710f5c6b9dc1f65d6399a4fd6e50daeaa32f
William Alfaro <walfaro@estudiantec.cr>
2022-10-07 04:02:53 -0600

    angelito

--- proyecto-1/Proyecto-Simpletron/src/main/java/tec/poo/simpletron/Terminal.java
+++ proyecto-1/Proyecto-Simpletron/src/main/java/tec/poo/simpletron/Terminal.java
@@ -1,12 +1,16 @@
 package tec.poo.simpletron;
-
+/**
+ *William Gerardo Alfaro Quiros – 2022437996
+ *
+ * Angel Gabriel Vargas Varela - 2021080292
+ */
 public class Terminal {
     public Terminal() {
     }
 
     public static void main(String[] args) {
         Simpletron LMS = new Simpletron();
-        LMS.run();
         System.out.println("Simplretron [version 1.0.0]\n");
+        LMS.execute();
     }
 }
